77f1e98b4cf80026227167fc3565dddc838ec634
AiziChen <1098679804@qq.com>
2022-10-13 09:02:49 +0800

    CipherController: add encrypt body function

--- src/main/java/com/cozz/deyou/controller/CipherController.java
+++ src/main/java/com/cozz/deyou/controller/CipherController.java
@@ -5,12 +5,19 @@ import com.cozz.deyou.cipher.AESUtils;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("api/cipher")
 @CrossOrigin(origins = "*")
 public class CipherController {
-    @PostMapping(value = "/api/cipher/decrypt-token")
+    @PostMapping(value = "/decrypt-token")
     @ResponseBody
     public String decryptToken(@RequestBody String enToken) {
         String midDecryptedToken = AESUtils.m3394c(enToken);
         return NativeDecrypt.decrypt(midDecryptedToken);
+    }
+
+    @PostMapping(value="/encrypt-body")
+    @ResponseBody
+    public String encryptBody(@RequestBody String body) throws Exception {
+        return AESUtils.m3399a(body, "d7b85f6e214abcda");
     }
 }
